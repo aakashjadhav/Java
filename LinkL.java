6c64013563b267721f690d7e8b92164a7a5c022b
Aakash <aakashjadhav459@gmail.com>
2018-08-18 19:45:08 +0530

    2nd

--- LinkL.java
+++ LinkL.java
@@ -1,7 +1,8 @@
 class LinkL
 {
-	Node head;
-	static class Node//so that this class can be accessed by main()
+Node head;
+
+	static class Node//made static so that it can be accessed by main()
 	{
 		int data;
 		Node next;
@@ -9,6 +10,17 @@ class LinkL
 		{
 			data=d;
 			next=null;
-		}
+		}	
 	}
+public static void main(String arg[])
+{
+	LinkL llist=new LinkL();
+	
+	llist.head=new Node(1);
+	Node second=new Node(2);
+	Node third=new Node(3);
+
+	llist.head.next=second;
+	second.next=third;
+}
 }
