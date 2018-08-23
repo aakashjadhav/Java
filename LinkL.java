9fd57d95048395f59d8bd0381ec598dc9df6c4d6
Aakash <aakashjadhav459@gmail.com>
2018-08-23 20:26:54 +0530

    Juz Check

--- LinkL.java
+++ LinkL.java
@@ -2,22 +2,22 @@ class LinkL
 {
 Node head;
 
-	static class Node
-	{
-		int data;
-		Node next;
-		
-		Node(int d)
-		{
-			data=d;
-			next=null;
-		}
-	}
-
-public void insertAtBeginning(int new_data)//insert data at beginning
+static class Node
 {
-Node new_node=new Node(new_data);
+int data;
+Node next;
 
+Node(int d)
+{
+data=d;
+next=null;
+}
+}
+
+public void insertAtStart(int new_data)
+{
+
+Node new_node=new Node(new_data);
 new_node.next=head;
 head=new_node;
 }
@@ -27,7 +27,7 @@ public void insertAfter(Node previous_node,int new_data)
 
 if(previous_node==null)
 {
-System.out.println("Previous Node cannot be Null");
+System.out.println("Previous node cannot be null");
 return;
 }
 
@@ -40,20 +40,20 @@ previous_node.next=new_node;
 public void insertAtEnd(int new_data)
 {
 Node new_node=new Node(new_data);
-Node n=head,temp=null;
-
 if(head==null)
 {
-head=new Node(new_data);
+head=new_node;
 return;
 }
+
 new_node.next=null;
 
-while(n.next!=null)
-n=n.next;
-n.next=new_node;
+Node last;
+last=head;
+while(last.next!=null)
+last=last.next;
 
-
+last.next=new_node;
 }
 
 public void printList()
@@ -62,47 +62,29 @@ Node n=head;
 
 while(n!=null)
 {
-	System.out.println("Data is "+n.data);
-	n=n.next;
-}
+System.out.println("Node is "+n.data);
+n=n.next;
 }
 
+}
 
 public static void main(String arg[])
 {
-LinkL llist=new LinkL();
+LinkL llink=new LinkL();
 
-llist.head=new Node(1);
+llink.head=new Node(1);
 Node second=new Node(2);
 Node third=new Node(3);
 
-llist.head.next=second;
+llink.head.next=second;
 second.next=third;
 
-//System.out.println("Link list before Inserting at Beginning");
-//llist.printList();
+llink.printList();
+//llink.insertAtStart(4);
+//llink.insertAfter(second,7);
+llink.insertAtEnd(8);
 
-//System.out.println("Link list after Inserting at Beginning");
-//llist.insertAtBeginning(4);
-//llist.printList();
-
-//System.out.println("Link list after inserting node after a specific node");
-//llist.printList();
-
-//System.out.println("Link list before inserting node after a specific node");
-//llist.insertAfter(second,4);
-//llist.printList();
-
-
-System.out.println("Link list before inserting node at End");
-llist.printList();
-
-System.out.println("Link list after inserting node at End");
-llist.insertAtEnd(7);
-llist.printList();
-
-
-
+System.out.println("\n");
+llink.printList();
 }
-
 }
